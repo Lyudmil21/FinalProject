4d305feed1df9f6c54f7b8adb3727b14fda51396
Людмил Николаев <ludmil_2003@abv.bg>
2026-04-21 23:43:32 +0300

    feat: meals cards fix

--- scripts/meals.js
+++ scripts/meals.js
@@ -9,14 +9,14 @@ export async function loadMeals(search = "") {
     renderMeals(meals);
 }
 
-function renderMeals(meals) {
+function renderMeals(meals, animate = true) {
     mealList.innerHTML = "";
 
     meals.forEach((meal, index) => {
         const li = document.createElement("li");
 
         li.innerHTML = `
-            <div class="card reveal">
+            <div class="card ${animate ? "reveal" : ""}">
                 <h3>${meal.strMeal}</h3>
                 <img src="${meal.strMealThumb}">
                 <p>${meal.strCategory || ""}</p>
@@ -24,13 +24,25 @@ function renderMeals(meals) {
         `;
 
         const card = li.querySelector(".card");
+
         card.addEventListener("click", () => {
             openModal(meal.idMeal, index, meals);
         });
 
         mealList.appendChild(li);
 
-        observer.observe(card);
-        card.style.transitionDelay = `${index * 0.02}s`;
+        if (animate) {
+            observer.observe(card);
+            card.style.transitionDelay = `${index * 0.02}s`;
+        } else {
+            card.classList.add("show"); 
+        }
     });
 }
+
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.trim();
+    loadMeals(value);
+});
